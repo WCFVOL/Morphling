58da26f395ee2b7e091bb28f0071aea382144bda
WCFVOL <1041875526@qq.com>
2018-09-28 20:43:11 +0800

    nav

--- src/main/java/com/wcfvol/rpc/monitoring/frontend/src/App.vue
+++ src/main/java/com/wcfvol/rpc/monitoring/frontend/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+  <el-header>Header</el-header>
+  <el-main>Main</el-main>
+</el-container>
   </div>
 </template>
 
